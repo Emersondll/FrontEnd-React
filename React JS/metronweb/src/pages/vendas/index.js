336e2f3afe724cdd611c78e56311320bd284ebd5
Emerson <>
2019-04-24 11:23:28 -0300

    Criacao de Tabs na camada Vendas

--- React JS/metronweb/src/pages/vendas/index.js
+++ React JS/metronweb/src/pages/vendas/index.js
@@ -1,13 +1,64 @@
 import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
 import Head from "../../header";
 import Foot from "../../footer";
 
+function TabContainer(props) {
+  return props.children;
+}
+
 export default class Vendas extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+  }
+
+  handleChangeTab = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
+    const { value } = this.state;
     return (
-      <div>
+      <div className="vendas">
         <Head />
-        Vendas
+        <div className="tab-clientes">
+          <AppBar position="static">
+            <Tabs value={value} onChange={this.handleChangeTab}>
+              <Tab label="Listar Vendas" />
+              <Tab label="Listar Produtos" />
+              <Tab label="Listar Reservados" />
+              <Tab label="Listar Histórico" />
+            </Tabs>
+          </AppBar>
+
+          {value === 0 && (
+            <TabContainer>
+              <div className="list-sales">Listar Vendas</div>
+            </TabContainer>
+          )}
+          {value === 1 && (
+            <TabContainer>
+              <div className="list-products">Listar Produtos</div>
+            </TabContainer>
+          )}
+          {value === 2 && (
+            <TabContainer>
+              <div className="list-products">Listar Reservados</div>
+            </TabContainer>
+          )}
+          {value === 3 && (
+            <TabContainer>
+              <div className="list-products">Listar Histórico</div>
+            </TabContainer>
+          )}
+        </div>
+        <br />
         <Foot />
       </div>
     );
